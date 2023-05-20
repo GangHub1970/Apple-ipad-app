1390a5b45444d4a303c7325c5a7aec90ded76245
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-05-20 18:28:33 +0900

    feat: Apply Animation to Search Button
    - 검색 버튼을 누르면 검색창의 요소들이 순차적으로 나타나는 애니메이션 추가
    - 돋보기 버튼을 누르면 나타나는 검색창 생성

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -26,4 +27,56 @@ function showBasket() {
 
 function hideBasket() {
   basketEl.classList.remove("show");
+}
+
+// 검색
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
+
+searchStarterEl.addEventListener("click", showSearch);
+
+searchCloserEl.addEventListener("click", hideSearch);
+
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add("searching");
+  // html 태그 요소를 선택하려면 document.documentElement를 사용한다.
+  document.documentElement.classList.add("fixed");
+
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${(index * 0.4) / headerMenuEls.length}s`;
+  });
+
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = `${(index * 0.4) / searchDelayEls.length}s`;
+  });
+
+  // input요소가 0.6초 이후에 나타나도록 애니메이션을 주었기 때문에 0.6초 뒤에 focus되도록 해준다.
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
+}
+
+function hideSearch() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${(index * 0.4) / headerMenuEls.length}s`;
+  });
+
+  searchDelayEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${(index * 0.4) / searchDelayEls.length}s`;
+  });
+  searchDelayEls.reverse();
+
+  // 검색창이 닫힐 때 검색창을 초기화해준다.
+  searchInputEl.value = "";
 }
